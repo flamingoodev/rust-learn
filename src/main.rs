7cda79e24a8467f07c4814cee28f872829191a31
FLAMINGO <flamingodev@outlook.com>
2021-01-05 00:06:07 +0800

    update size

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use rust_learn::syntax::t_iter;
 use rust_learn::syntax::t_queue;
 use rust_learn::syntax::t_string;
 use rust_learn::syntax::t_thread;
+use rust_learn::syntax::t_size;
 use rust_learn::syntax::t_trait::{NewsArticle, Summary, Summary1, Tweet};
 
 fn main() {
@@ -93,4 +94,5 @@ fn main() {
     }
     println!("size = {}, data = {:#?}", queue.size(), queue);
     println!("---------------------Queue----------------------");
+    t_size::test_size();
 }
--- src/syntax/t_size.rs
+++ src/syntax/t_size.rs
@@ -1,4 +1,5 @@
 use std::mem::size_of_val;
+use std::mem::size_of;
 
 struct Test<'a> {
     _a: i32,
@@ -8,5 +9,10 @@ struct Test<'a> {
 pub fn test_size() {
     let array: [u8; 10] = [1; 10];
     let slice = &array;
+    println!("&i32 size = {}", size_of::<&i32>());
+    println!("&i64 size = {}", size_of::<&i64>());
+    println!("i32 size = {}", size_of::<i32>());
+    println!("i64 size = {}", size_of::<i64>());
+    println!("&u8 size = {}", size_of::<&u8>());
     println!("array size = {}", size_of_val(slice));
 }
