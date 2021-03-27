379f76439ba4b0457f99a2e13c0773e7deb7b02f
FLAMINGO <flamingodev@outlook.com>
2021-03-27 17:11:31 +0800

    update op

--- src/syntax/t_function.rs
+++ src/syntax/t_function.rs
@@ -12,6 +12,14 @@ fn product(a: i32, b: i32) -> i32 {
     a * b
 }
 
+// 高阶函数
+fn mat(op: fn(i32, i32) -> i32, a: i32, b: i32) -> i32 {
+    op(a, b)
+}
+
+fn hello() {
+    println!("Hello")
+}
 /// CTFE机制，编译时函数执行（Compile-Time Function Execution，CFTE）
 const fn init_len() -> usize {
     5
@@ -28,4 +36,18 @@ fn t_math() {
 fn t_init_len() {
     let arr = [0; init_len()];
     println!("{:?}", arr);
+}
+
+#[test]
+fn t_op() {
+    let r = mat(sum, 1, 2);
+    let x = &42;
+    // this produces something like '0x7f06092ac6d0'
+    let address = format!("{:p}", x);
+
+    let fn_ptr = hello;
+    let addr = format!("{:p}", &fn_ptr);
+    println!("{}", address);
+    println!("{}", addr);
+    println!("{}", r);
 }
--- src/syntax/t_generic.rs
+++ src/syntax/t_generic.rs
@@ -1,3 +1,6 @@
+use std::ops::Mul;
+
+#[derive(Debug)]
 pub struct Point<T> {
     pub x: T,
     pub y: T,
@@ -9,8 +12,42 @@ impl<T> Point<T> {
     }
 }
 
+/// 为结构体实现了方法
 impl Point<usize> {
     pub fn usize_x(&self) -> &usize {
         &self.x
     }
+    pub fn show(&self) {
+        println!("{:?}", self)
+    }
+}
+
+/// 定义了函数
+/// Mul有理数限定
+fn square<T: Mul + Mul<Output = T>>(x: T, y: T) -> T {
+    x * y
+}
+
+fn square1<T: Mul<T, Output = T>>(x: T, y: T) -> T {
+    x * y
+}
+
+#[test]
+fn test_square() {
+    let s = square(10, 20);
+    let s1 = square1(10, 20);
+    let f = square1(10.2, 20.44);
+    let f1 = square1(10.2, 20.44);
+    assert_eq!(s, s1);
+    assert_eq!(f, f1);
+    // turbofish operation
+    let r = square::<u32>(37, 41);
+    let r1 = square::<f32>(37.7, 41.2);
+    println!("{}", r);
+    println!("{}", r1);
+    let p = Point { x: 5, y: 10 };
+    println!("p.x = {}", p.usize_x());
+    p.show();
+    Point::show(&p);
+    println!("p.x = {}", p.x());
 }
