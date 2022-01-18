dc8beb77eab571030676b1174da4a69ddf98c985
flamingo <flamingodev@outlook.com>
2022-01-19 00:29:20 +0800

    feat: update generic

--- src/collections/t_vector.rs
+++ src/collections/t_vector.rs
@@ -1,3 +1,5 @@
+use std::fmt;
+
 pub fn vec_new() {
     // use Vec::new()
     let mut v: Vec<i32> = Vec::new();
@@ -63,6 +65,43 @@ fn if_let() -> u8 {
     binary
 }
 
+fn vec_slice() {
+    let v = vec![1, 2, 3, 4];
+
+    // Vec 实现了 Deref，&Vec<T> 会被自动解引用为 &[T]，符合接口定义
+    print_slice(&v);
+    // 直接是 &[T]，符合接口定义
+    print_slice(&v[..]);
+
+    // &Vec<T> 支持 AsRef<[T]>
+    print_slice1(&v);
+    // &[T] 支持 AsRef<[T]>
+    print_slice1(&v[..]);
+    // Vec<T> 也支持 AsRef<[T]>
+    print_slice1(v);
+
+    let arr = [1, 2, 3, 4];
+    // 数组虽没有实现 Deref，但它的解引用就是 &[T]
+    print_slice(&arr);
+    print_slice(&arr[..]);
+    print_slice1(&arr);
+    print_slice1(&arr[..]);
+    print_slice1(arr);
+}
+
+// 注意下面的泛型函数的使用
+fn print_slice<T: fmt::Debug>(s: &[T]) {
+    println!("{:?}", s);
+}
+
+fn print_slice1<T, U>(s: T)
+    where
+        T: AsRef<[U]>,
+        U: fmt::Debug,
+{
+    println!("{:?}", s.as_ref());
+}
+
 #[test]
 fn test_if_let() {
     assert_eq!(if_let(), 1);
@@ -87,4 +126,9 @@ fn while_let() {
 #[test]
 fn test_while_let() {
     while_let()
+}
+
+#[test]
+fn test_slice() {
+    vec_slice()
 }
--- src/syntax/t_generic.rs
+++ src/syntax/t_generic.rs
@@ -1,9 +1,81 @@
 use std::ops::Mul;
+use std::sync::Arc;
+use std::any::type_name;
 
 #[derive(Debug)]
 pub struct Point<T> {
     pub x: T,
     pub y: T,
+}
+
+/// 带有默认范性类型的结构体
+#[derive(Debug)]
+pub struct PointDefault<T = u32> {
+    pub x: T,
+    pub y: T,
+}
+
+impl<T> PointDefault<T> {
+    pub fn x(&self) -> &T {
+        &self.x
+    }
+}
+
+/// 默认实现
+impl PointDefault {
+    pub fn y(&self) {
+        test_type(self.y);
+        println!("{}", &self.y)
+    }
+}
+
+/// 带有默认范性类型的结构体
+/// 默认泛型类型参数，表示如果不显示指定泛型类型，就默认泛型类型为Self。
+/// 当使用泛型类型参数时，可以为泛型指定一个默认的具体类型。如果默认类型就足够的话，
+/// 这消除了为具体类型实现 trait 的需要。
+/// 为泛型类型指定默认类型的语法是在声明泛型类型时使用 <PlaceholderType=ConcreteType>。
+pub trait Watch<Inner = String> {
+    type Item;
+    fn inner(&self) -> Option<Self::Item>;
+    fn info(&self) -> Inner;
+}
+
+struct A {
+    data: i32,
+}
+
+impl Watch<i32> for A {
+    type Item = i32;
+    fn inner(&self) -> Option<Self::Item> {
+        Some(self.data)
+    }
+    fn info(&self) -> i32 {
+        println!("A inner is {}", self.data);
+        self.data
+    }
+}
+
+struct B {
+    data: String,
+}
+
+/// 默认实现
+impl Watch for B {
+    type Item = String;
+    fn inner(&self) -> Option<Self::Item> {
+        Some(self.data.clone())
+    }
+    fn info(&self) -> String {
+        println!("B inner is {}", self.data);
+        self.data.clone()
+    }
+}
+
+fn main() {
+    let a = A { data: 10 };
+    let b = B { data: String::from("B") };
+    assert_eq!(10, a.info());
+    assert_eq!(Some(String::from("B")), b.inner());
 }
 
 impl<T> Point<T> {
@@ -24,12 +96,16 @@ impl Point<usize> {
 
 /// 定义了函数
 /// Mul有理数限定
-fn square<T: Mul + Mul<Output = T>>(x: T, y: T) -> T {
+fn square<T: Mul + Mul<Output=T>>(x: T, y: T) -> T {
     x * y
 }
 
-fn square1<T: Mul<T, Output = T>>(x: T, y: T) -> T {
+fn square1<T: Mul<T, Output=T>>(x: T, y: T) -> T {
     x * y
+}
+
+fn test_type<T>(_: T) {
+    println!("{:?}", { type_name::<T>() });
 }
 
 #[test]
@@ -50,4 +126,9 @@ fn test_square() {
     p.show();
     Point::show(&p);
     println!("p.x = {}", p.x());
+    let p1 = PointDefault::<u32> { x: 5, y: 10 };
+    let p2 = PointDefault { x: 5, y: 10 };
+    test_type(p1.x);
+    test_type(p2.x());
+    p2.y()
 }
