b6870ad77216aad66d12cb6058af3e0b62a8626e
flamingo <flamingodev@outlook.com>
2022-05-10 23:11:07 +0800

    feat: update enum

--- src/syntax/t_enum.rs
+++ src/syntax/t_enum.rs
@@ -1,7 +1,43 @@
+use strum::{Display, EnumCount, EnumDiscriminants, EnumString};
+
+use std::str::FromStr;
+
 enum SpreadsheetCell {
     Int(i32),
     Float(f64),
     Text(String),
+}
+
+#[derive(Debug, Eq, PartialEq, EnumString, Display, EnumCount, EnumDiscriminants)]
+enum Color {
+    /// Random Docs
+    #[strum(to_string = "RedRed")]
+    Red,
+    #[strum(serialize = "b", to_string = "blue")]
+    Blue { hue: usize },
+    #[strum(serialize = "y", serialize = "yellow")]
+    Yellow,
+    White,
+    #[strum(disabled)]
+    Green(String),
+}
+
+#[test]
+fn t_strum() {
+    let color: String = Color::Red.to_string();
+    println!("{}", color); // Red
+
+    let new_color = "y";
+    let s = Color::try_from(new_color).unwrap();
+    println!("{:?}", s);
+
+    let new_color1 = "White";
+    let s1 = Color::try_from(new_color1).unwrap();
+    println!("{:?}", s1);
+
+    let new_color2 = "White";
+    let s2 = Color::from_str(new_color2).unwrap();
+    println!("{:?}", s2);
 }
 
 pub fn t_enum() {
